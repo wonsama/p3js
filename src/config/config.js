14315d36141ab75c4d9b80d07f55c651135c6e1f
wonsama <wonsama@kakao.com>
2022-03-17 17:24:07 +0900

    part3
    이미지 추가 및 컨트롤

--- src/config/config.js
+++ src/config/config.js
@@ -4,8 +4,8 @@ import Scene2 from "../scenes/Scene2";
 export default {
   type: Phaser.AUTO,
   parent: "phaser-example",
-  width: 800,
-  height: 600,
+  width: 256,
+  height: 272,
   scene: [Scene1, Scene2], // 자동으로 this.scene.start 한 것과 동일, 맨 앞의 scene 이 기동됨
   backgroundColor: "#000000", // 기본 검정색(#000000) 임
 };
